5b88052e5a40d958a55a6db1f5406e9210462ddd
Nicolas Corbet <nicolas.corbet@corbet-it.fr>
2021-01-27 10:33:39 +0100

    added terraform IaC and also to circleci pipeline

---
+++ infrastructure/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "nc-app-cinema-tf-state"
+    key    = "app-cinema.tfstate"
+    region = "eu-west-3"
+    # encrypt = true
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    ManagedBy   = "Terraform"
+    Owner       = "Nicolas Corbet"
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  default = "nc-react-cinema"
+}
+
+variable "project" {
+  default = "cinema-react-app"
+}
+
+variable "cloudfront_certificate" {
+  default = "us-east-1:039509991122:certificate/bcc1be84-3361-41a7-8d89-71c5923e4d88"
+}
