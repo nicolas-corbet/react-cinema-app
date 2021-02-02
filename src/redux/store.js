768ab7c5c15ba432516c1f4e188340f10d6ab9f1
Nicolas Corbet <nicolas.corbet@corbet-it.fr>
2021-02-02 10:45:13 +0100

    js files prettied

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,6 +7,10 @@ import rootReducers from './reducers';
 const initialState = {};
 const middleware = [thunk];
 
-export const store = createStore(rootReducers, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+export const store = createStore(
+  rootReducers,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
